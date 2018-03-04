1da2d947ec2451e70d2d6b0ed99c04e8ff5dbfa9
Sebastian Köln <sebk@rynx.org>
2018-03-04 19:59:48 +0100

    fixing stuff

---
+++ bullet_macros/src/lib.rs
@@ -0,0 +1,26 @@
+#![feature(proc_macro)]
+
+extern crate bullet;
+extern crate proc_macro;
+
+
+use proc_macro::TokenStream;
+use bullet::vm;
+use bullet::prelude::{NodeRc, Builder};
+
+fn parse(t: TokenStream) -> NodeRc {
+    let input = t.to_string();
+    let builder = Builder::new();
+    let node = builder.parse(&input).expect("failed to parse");
+    node
+}
+
+#[proc_macro]
+pub fn math(t: TokenStream) -> TokenStream {
+    vm::syn::syn(parse(t)).to_string().parse().expect("failed to parse")
+}
+
+#[proc_macro]
+pub fn math_asm(t: TokenStream) -> TokenStream {
+    vm::simd::simd_asm(&[parse(t)], &["x"]).to_string().parse().expect("failed to parse")
+}
